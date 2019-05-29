a33878f2db1a6ba9a035e6344b0f11a07e0e1323
André Luiz <xester22@hotmail.com>
2019-05-28 21:12:23 -0300

    segunda tentativa de automatizar o desafio da Montreal

--- cucumber/features/support/hooks.rb
+++ cucumber/features/support/hooks.rb
@@ -1,9 +1,10 @@
-
-
-After do |scenario|
-  # tira um print por favor
-  file_name = scenario.name.tr(' ', '_').downcase!
-  shot = "logs/shots/#{file_name}.png"
-  page.save_screenshot(shot)
-  embed(shot, 'image/png', 'Clique aqui para ver o print do teste')
-end
+# def initialize(browser)
+#   @browser = browser
+# end
+# After do |scenario|
+#   # tira um print por favor
+#   file_name = scenario.name.tr(' ', '_').downcase!
+#   shot = "logs/shots/#{file_name}.png"
+#   browser.save_screenshot(shot)
+#   embed(shot, 'image/png', 'Clique aqui para ver o print do teste')
+# end
